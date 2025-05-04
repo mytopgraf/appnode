a88d4a473f4ab6f4e31410f9253adf4e59a89589
mytopgraf <mytopgraf@gmail.com>
2025-05-04 11:49:07 +0300

    Updated-2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 export default async ({ req, res, log, error }) => {
-  return res.send('Hi there');
+  if (req.method == 'GET') {
+    return res.send('Hi there');
+  }
+
+  if (req.method == 'POST') {
+    return res.json({
+      'sendData': req.body,
+      'DOGENV': process.env.DOGENV,
+      'CATENV': process.env.CATENV
+    })
+  }
 };
